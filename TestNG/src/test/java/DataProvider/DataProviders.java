4348bd236e0820a4ca9102d20032180cdba1cc7a
roshanshetty28 <roshanshetty2816@gmail.com>
2024-02-17 11:55:04 +0530

    Added Excel Reading functionality

--- TestNG/src/test/java/DataProvider/DataProviders.java
+++ TestNG/src/test/java/DataProvider/DataProviders.java
@@ -12,13 +12,18 @@ import Utility.Util;
 public class DataProviders {
 	@DataProvider(name = "loginData")
 	public static Object[][] loginDataProvider() throws IOException, ParseException {
-		JSONArray jsonArray = Util.JSONArrayExtractor();
-		Object[][] resObj = new Object[jsonArray.size()][2];
-		for (int i = 0; i < jsonArray.size(); i++) {
-			JSONObject userDetails = (JSONObject) jsonArray.get(i);
-			resObj[i][0] = userDetails.get("username");
-			resObj[i][1] = userDetails.get("password");
-		}
-		return resObj;
+
+		// Extract JSON data
+//		JSONArray jsonArray = Util.JSONArrayExtractor();
+//		Object[][] resObj = new Object[jsonArray.size()][2];
+//		for (int i = 0; i < jsonArray.size(); i++) {
+//			JSONObject userDetails = (JSONObject) jsonArray.get(i);
+//			resObj[i][0] = userDetails.get("username");
+//			resObj[i][1] = userDetails.get("password");
+//		}
+//		return resObj;
+
+		// Extract Data from Excel
+		return Util.ExcelReader("loginData");
 	}
 }
--- TestNG/src/test/java/Utility/Util.java
+++ TestNG/src/test/java/Utility/Util.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.*;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -26,5 +28,24 @@ public class Util extends BaseClass {
 		JSONParser parser = new JSONParser();
 		Object obj = parser.parse(file);
 		return (JSONArray) obj;
+	}
+
+	public static Object[][] ExcelReader(String sheetName) throws EncryptedDocumentException, IOException {
+		Workbook workbook = WorkbookFactory
+				.create(new File(".//src//test/resources//testData//ExcelFiles//LoginData.xlsx"));
+		Sheet sheet = workbook.getSheet(sheetName);
+		int totalRows = sheet.getLastRowNum();
+		Row headerRow = sheet.getRow(0);
+		int totalColumns = headerRow.getLastCellNum();
+
+		Object[][] res = new Object[totalRows][totalColumns];
+		DataFormatter formatter = new DataFormatter();
+
+		for (int i = 1; i <= totalRows; i++) {
+			for (int j = 0; j < totalColumns; j++) {
+				res[i - 1][j] = formatter.formatCellValue(sheet.getRow(i).getCell(j));
+			}
+		}
+		return res;
 	}
 }
